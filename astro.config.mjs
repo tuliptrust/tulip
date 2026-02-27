db0cdfcb52ece8fc3fb5cdd5360e847997bfe156
rk <rk54rk@hotmail.com>
2026-02-28 05:05:07 +0800

    configure base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  base: "./",
+  output: "static",
+  integrations: [react()],
 });
