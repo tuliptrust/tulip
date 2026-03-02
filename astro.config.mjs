51fcbc020e5a1d6302c7757ed29de3b0935f8bbe
rk <rk54rk@hotmail.com>
2026-03-02 13:31:56 +0800

    fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  build: {
-    assets: "relative",
-  },
   integrations: [react()],
 });
